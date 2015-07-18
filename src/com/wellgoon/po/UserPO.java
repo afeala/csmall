fd8e91ab7f6320fc706594df0d0a212ee06f4841
杨景辉 <jh.yang@csmall.com>
2015-07-18 09:44:34 +0800

    test

---
+++ src/com/wellgoon/po/UserPO.java
@@ -0,0 +1,46 @@
+package com.wellgoon.po;
+
+/**
+ * Created by Administrator on 2015/6/29.
+ */
+public class UserPO {
+    private String id;
+    private String username;
+    private String password;
+    private String nickname;
+
+    public void UserPO(){
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
---
+++ src/com/wellgoon/utils/StringUtils.java
@@ -0,0 +1,7 @@
+package com.wellgoon.utils;
+
+/**
+ * Created by Administrator on 2015/6/29.
+ */
+public class StringUtils {
+}
